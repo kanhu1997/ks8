ddae1825d171ba466c2467868830644fa3defff4
Charan <kanhucharandash55@gamil.com>
2025-04-17 18:47:31 +0530

    modifiy the .js file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('HelloDeployee in dev env on Kubernetes use Jenkins CICD!');
+    res.send('"Deployment in the development environment on Kubernetes is managed using Jenkins CI/CD."!');
 });
 
 app.listen(port, () => {
